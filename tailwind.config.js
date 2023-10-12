926b32a08f59ec9a4603eadde4df24e3eee2f284
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-10-11 22:58:24 -0400

    revert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./application/*.html",
-            "./general/*.html",
-            "./pet/*.html",
-          "./application/conversation.html"],
+    content: [
+    './public/**/*.{html,js}',
+    '*.{html,js}'
+],
   theme: {
     extend: {
       colors: {
