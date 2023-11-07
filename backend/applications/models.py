ffb5bbf6938adf7e73c7eefe1feb778ba0762f37
Sanzhar Shamshiyev <sanpos.kz@gmail.com>
2023-11-07 16:47:42 -0500

    new structure

---
+++ backend/applications/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+from petlistings.models import PetListing
+
+# Create your models here.
+
+class Application(models.Model):
+    date = models.DateTimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='applications',
+                             on_delete=models.CASCADE)
+    pet = models.ForeignKey(PetListing, on_delete=models.CASCADE, null=True)
+
+class ApplicationAnswer(models.Model):
+    application = models.ForeignKey(Application,
+                                    on_delete=models.CASCADE,
+                                    related_name="answers")
+    question_num = models.IntegerField(blank=False, null=False)
+    answer = models.TextField()
---
+++ backend/petlistings/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PetlistingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'petlistings'
