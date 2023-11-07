0533b4adfe4a93448876bcc42705171d88cec6d7
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-11-07 01:06:26 -0500

    completed Application and ApplicationAnswer models

--- petpal/applications/admin.py
+++ petpal/applications/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Application, ApplicationAnswer
 
 # Register your models here.
+
+admin.site.register(Application, ApplicationAnswer)
--- petpal/applications/models.py
+++ petpal/applications/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+
+from ..petlistings import PetListing
 
 # Create your models here.
+
+class Application(models.Model):
+    date = models.DateTimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                             related_name='applications', 
+                             on_delete=models.CASCADE)
+    pet = models.ForeignKey(PetListing, on_delete=models.CASCADE)
+
+class ApplicationAnswer(models.Model):
+    application = models.ForeignKey(Application, 
+                                    on_delete=models.CASCADE, 
+                                    related_name="answers")
+    question_num = models.IntegerField(blank=False, null=False)
+    answer = models.TextField()
+    
