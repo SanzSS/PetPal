cf4d235d21693148a52e1ac310c0095fffd62199
dianzrong <dianrong2012@gmail.com>
2023-11-07 15:38:46 -0500

    add date and fix related_names

--- petpal/applications/models.py
+++ petpal/applications/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.conf import settings
 
@@ -14,15 +15,16 @@ class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="comments", 
                              on_delete=models.CASCADE)
     content = models.TextField()
+    creation_date = models.DateTimeField(default=datetime.now)
 
     # what the comment is left on
     type = models.CharField(choices=TYPE_CHOICES)
 
     # if the comment is on an application, then this is null 
-    shelter = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="shelter", 
+    shelter = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="comments", 
                                 blank=True, null=True, on_delete=models.CASCADE)
 
     # if the comment is on a shelter, then this is null
-    application = models.ForeignKey(Application, related_name="application",
+    application = models.ForeignKey(Application, related_name="comments",
                                     blank=True, null=True, on_delete=models.CASCADE)
     
