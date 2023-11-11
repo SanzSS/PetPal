442dd0fe9be8b281dcd1e430a9a5b2e6e988b5ee
dianzrong <dianrong2012@gmail.com>
2023-11-10 20:04:55 -0500

    logic done

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -8,25 +8,23 @@ from .models import Comment, Review
 # single serializers
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    application = serializers.SlugRelatedField(read_only=True, slug_field='pet')
 
     class Meta:
         model = Comment
-        fields = ['user', 'content', 'application']
+        fields = ['user', 'content']
 
 class ReviewSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(read_only=True, slug_field='name')
-    shelter = serializers.SlugRelatedField(read_only=True, slug_field='name')
 
     class Meta:
         model = Review
-        fields = ['user', 'content', 'shelter']
+        fields = ['user', 'content']
 
 # list serializers
-class CommentListSerializer(serializers.ModelSerializer):
-    application = serializers.PrimaryKeyRelatedField(read_only=True)
-    comments = serializers.ListField(child=CommentSerializer())
+# class CommentListSerializer(serializers.ModelSerializer):
+#     application = serializers.PrimaryKeyRelatedField(read_only=True)
+#     comments = serializers.ListField(child=CommentSerializer())
 
-class ReviewListSerializer(serializers.ModelSerializer):
-    shelter = serializers.PrimaryKeyRelatedField(read_only=True)
-    comments = serializers.ListField(child=ReviewSerializer())
+# class ReviewListSerializer(serializers.ModelSerializer):
+#     shelter = serializers.PrimaryKeyRelatedField(read_only=True)
+#     comments = serializers.ListField(child=ReviewSerializer())
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,38 +1,68 @@
-from django.shortcuts import render, get_list_or_404
+from django.http import JsonResponse
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
+from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Comment, Review
-from .serializers import CommentSerializer, CommentListSerializer, ReviewSerializer, ReviewListSerializer
+from .serializers import CommentSerializer, ReviewSerializer
 
 # Create your views here.
 class ApplicationView(ListCreateAPIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+    serializer_class = CommentSerializer
     
-    def get_serializer_class(self):
-        if self.request.method in ['GET']:
-            return CommentSerializer
-        else:
-            return CommentListSerializer
-    
-    # want to get all comments on an application
+    # want to get first 10 comments on an application
     def get_queryset(self):
+        # get the application
         application_id = self.kwargs['application_id']
-        application_comments = get_list_or_404(Comment, application_id=application_id)
-        return application_comments
+        start = self.kwargs['start']
+        comments_queryset = get_list_or_404(Comment, application_id=application_id)
+
+        sample_comment = comments_queryset[0]
+
+        # if there aren't 4 left after start
+        if start + 5 > len(comments_queryset):
+            return comments_queryset[start:]
+        else:
+            end = start + 5
+            return comments_queryset[start:end]
+
 
 class ShelterView(ListCreateAPIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+    serializer_class = ReviewSerializer
     
-    def get_serializer_class(self):
-        if self.request.method in ['GET']:
-            return ReviewSerializer
-        else:
-            return ReviewListSerializer
     
     # want to get all comments on a shelter
     def get_queryset(self):
         shelter_id = self.kwargs['shelter_id']
-        shelter_views = get_list_or_404(Review, shelter_id=shelter_id)
-        return shelter_views
+        start = self.kwargs['start']
+        
+        review_queryset = get_list_or_404(Review, shelter_id=shelter_id).order_by("-creation_date")
+
+        # if there aren't 4 left after start
+        if start + 5 > len(review_queryset):
+            return review_queryset[start:]
+        else:
+            end = start + 5
+            return review_queryset[start:end]
+        # [0, 1, 2, 3, 4]
+    
+# helper function to detect if the client has permission to edit or view the application
+class ApplicationPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        application_id = self.kwargs['application_id']
+        comments_queryset = get_list_or_404(Comment, application_id=application_id)
+
+        application = comments_queryset[0].application
+        applicant = application.user
+
+        shelter = application.pet.shelter 
+
+        if self.request.user != applicant and self.request.user != shelter:
+            raise PermissionDenied("You do not have permission to create this object.")
+
+        return True
