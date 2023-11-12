451a4c861e2a13af8714af01f80aa6884af2a6c4
Sanzhar Shamshiyev <sanpos.kz@gmail.com>
2023-11-12 16:44:17 -0500

    draft

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,9 +1,16 @@
+from datetime import datetime
+
 from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 class Notification(models.Model):
 
-    content = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_id = models.PositiveIntegerField()
+    content = GenericForeignKey('content_type', 'content_id')
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notif')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
+    state = models.BooleanField(default=False)
+    creation_date = models.DateTimeField(default=datetime.now)
--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
 
+from django.views.generic import UpdateView, ListView
+from .models import Notification
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from rest_framework.response import Response
+from .serializers import NotificationSerializer
+from rest_framework.views import APIView
+
+
+
 # Create your views here.
+
+class UpdateApplicationNotif(UpdateView):
+    model = Notification
+
+    def get(self, request, *args, **kwargs):
+        notif = self.get_object()
+        notif.state = True
+        notif.save()
+        content = notif.content
+        return reverse_lazy('application', kwargs={'application_id': content.comments, })
+
+    def get_object(self, queryset=None):
+        key1 = self.kwargs.get('key1')
+        key2 = self.kwargs.get('key2')
+        notif = get_object_or_404(Notification, receiver=key1, sender=key2)
+        return notif
+
+class UpdateShelterNotif(UpdateView):
+    model = Notification
+
+    def get(self, request, *args, **kwargs):
+        notif = self.get_object()
+        notif.state = True
+        notif.save()
+        content = notif.content
+        return reverse_lazy('shelter', kwargs={'shelter_id': content.reviews, })
+
+    def get_object(self, queryset=None):
+        key1 = self.kwargs.get('key1')
+        key2 = self.kwargs.get('key2')
+        notif = get_object_or_404(Notification, receiver=key1, sender=key2)
+        return notif
+
+class NotifSeekerList(APIView):
+    model = Notification
+    def get_queryset(self):
+        queryset = Notification.objects.filter(receiver=self.kwargs.get('seeker_id')).order_by('-creation_date')
+        return queryset
+
+    def get(self, request, format=None):
+        serializer = NotificationSerializer(self.get_queryset(), many=True)
+        return Response(serializer.data)
+
+
+class NotifShelterList(APIView):
+    model = Notification
+    def get_queryset(self):
+        queryset = Notification.objects.filter(receiver=self.kwargs.get('shelter_id')).order_by('-creation_date')
+        return queryset
+
+    def get(self, request, format=None):
+        serializer = NotificationSerializer(self.get_queryset(), many=True)
+        return Response(serializer.data)
+
