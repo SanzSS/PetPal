03d84dffb32b0b4a0c9f481b0caac9ae65ebb2f1
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-11-12 16:36:58 -0500

    black isort

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from .views import AccountsCreate
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    path('', AccountsCreate.as_view(), name='account-create'),
+    path("", AccountsCreate.as_view(), name="account-create"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
+
 from .serializers import CreateUserSerializer
 
 # Create your views here.
+
 
 class AccountsCreate(CreateAPIView):
     serializer_class = CreateUserSerializer
