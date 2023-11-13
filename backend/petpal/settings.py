001b84119d6c6db8c06c81924d62d9c72e9b3520
Victor Yao <35826442+VictorYao17@users.noreply.github.com>
2023-11-12 19:05:12 -0500

    added media root to project settings.py

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ],
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
