d3cd1d7ae2e9b9760d746e5de60c1cb72875f29d
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-11-14 01:34:51 -0500

    404

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Application
+from .models import Application, PetListing
 from .serializers import ApplicationSerializer
 
 # Create your views here.
@@ -12,6 +12,11 @@ from .serializers import ApplicationSerializer
 class ApplicationsCreate(CreateAPIView):
     serializer_class = ApplicationSerializer
     permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        petlisting_id = self.kwargs.get('petlisting_id')
+        pet = get_object_or_404(PetListing, id=petlisting_id)
+        return super().post(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         petlisting_id = self.kwargs.get('petlisting_id')
