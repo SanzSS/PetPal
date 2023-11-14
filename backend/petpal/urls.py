ef42cbc6c79ab00cbbd7e8bf484594317de24389
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-11-14 00:07:54 -0500

    fix all admin panel and add new app

--- backend/petpal/urls.py
+++ backend/petpal/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('applications/', include('applications.urls', namespace='applications')),
 ]
