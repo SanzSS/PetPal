5d11882a8b099429eaed38f9897c25a9e6fb79f9
dianzrong <dianrong2012@gmail.com>
2023-11-14 21:58:48 -0500

    finished accounts get

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import permissions
 from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404, get_list_or_404
 from .serializers import CreateUserSerializer, UserSerializer, UpdateUserSerializer
@@ -11,6 +13,24 @@ from petlistings.models import PetListing
 from applications.models import Application
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+# helper function to detect if the client has permission to edit or view the application
+class ViewSeeker(permissions.BasePermission):
+    def has_permission(self, request, view):
+        user_id = view.kwargs['user_id']
+        user = get_object_or_404(User, id=user_id)
+
+        if user.user_type == "shelter":
+            return True
+        # shelter can only view seeker if seeker has an active application w a shelter
+        if view.request.user.user_type == "shelter":
+            print("y")
+            applications = Application.objects.filter(user=user, status=Application.Status.PENDING)
+            if applications.exists():
+                return True
+            print(applications)
+            print(Application.objects.all())
+        return False
 
 
 class AccountsCreate(CreateAPIView):
@@ -47,21 +67,10 @@ def delete(request, shelter_id):
 
 class GetAccount(RetrieveAPIView):
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, ViewSeeker]
     
     def get_object(self):
         user_id = self.kwargs['user_id']
         user = get_object_or_404(User, id=user_id)
-        
-        # anyone can view a shelter
-        if user.user_type == "shelter":
-            return user
-        # shelter can only view seeker if seeker has an active application w a shelter
-        if self.request.user.user_type == "shelter":
-            applications = Application.objects.filter(user=user, status=Application.Status.PENDING)
-            if applications.exists():
-                return user
-            return HttpResponse(status=403)
-
-        return HttpResponse(status=403)
+        return user
 
