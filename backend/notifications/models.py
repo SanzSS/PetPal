05648080a1bcd23410db2e8a3b04a7dba5c1fee9
Sanzhar Shamshiyev <sanpos.kz@gmail.com>
2023-11-14 19:24:36 -0500

    account stuff

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -8,7 +8,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 class Notification(models.Model):
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    content_id = models.PositiveIntegerField()
+    content_id = models.PositiveIntegerField(null=True)
     content = GenericForeignKey('content_type', 'content_id')
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notif')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-from django.views.generic import UpdateView, ListView
+from django.views.generic import UpdateView
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+
 from .models import Notification
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
@@ -12,56 +16,69 @@ from rest_framework.views import APIView
 
 # Create your views here.
 
-class UpdateApplicationNotif(UpdateView):
+class UpdateNotif(UpdateView):
     model = Notification
 
     def get(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return HttpResponse(status=401)
         notif = self.get_object()
         notif.state = True
         notif.save()
         content = notif.content
-        return reverse_lazy('application', kwargs={'application_id': content.comments, })
+        if notif.content_type.model == 'comment':
+            return reverse_lazy('application', kwargs={'application_id':
+                                                           content.comments,
+                                                       'start': 0})
+        if notif.content_type.model == 'review':
+            return reverse_lazy('shelter',
+                                kwargs={'shelter_id': content.reviews,
+                                        'start': 0})
 
     def get_object(self, queryset=None):
-        key1 = self.kwargs.get('key1')
-        key2 = self.kwargs.get('key2')
-        notif = get_object_or_404(Notification, receiver=key1, sender=key2)
+        key1 = self.request.user.pk
+        key2 = self.kwargs.get('notif_id')
+        notif = get_object_or_404(Notification, receiver=key1, pk=key2)
         return notif
 
-class UpdateShelterNotif(UpdateView):
-    model = Notification
-
-    def get(self, request, *args, **kwargs):
-        notif = self.get_object()
-        notif.state = True
-        notif.save()
-        content = notif.content
-        return reverse_lazy('shelter', kwargs={'shelter_id': content.reviews, })
-
-    def get_object(self, queryset=None):
-        key1 = self.kwargs.get('key1')
-        key2 = self.kwargs.get('key2')
-        notif = get_object_or_404(Notification, receiver=key1, sender=key2)
-        return notif
-
-class NotifSeekerList(APIView):
+class NotifList(APIView):
     model = Notification
     def get_queryset(self):
-        queryset = Notification.objects.filter(receiver=self.kwargs.get('seeker_id')).order_by('-creation_date')
+        queryset = Notification.objects.filter(receiver=self.request.user.pk).order_by('-creation_date')
         return queryset
 
     def get(self, request, format=None):
-        serializer = NotificationSerializer(self.get_queryset(), many=True)
-        return Response(serializer.data)
+        if request.user.is_authenticated:
+            queryset = self.get_queryset()
 
+            paginator = PageNumberPagination()
+            page = paginator.paginate_queryset(queryset, request)
 
-class NotifShelterList(APIView):
+            if page is not None:
+                serializer = NotificationSerializer(page, many=True)
+                return paginator.get_paginated_response(serializer.data)
+            else:
+                serializer = NotificationSerializer(queryset, many=True)
+                return Response(serializer.data)
+
+        else:
+            return Response({
+                                "detail": "You do not have permission to view these notifications."},
+                            status=status.HTTP_403_FORBIDDEN)
+
+class FilterUnread(NotifList):
     model = Notification
     def get_queryset(self):
-        queryset = Notification.objects.filter(receiver=self.kwargs.get('shelter_id')).order_by('-creation_date')
+        queryset = Notification.objects.filter(
+            receiver=self.request.user.pk, state=False).order_by('-creation_date')
         return queryset
 
-    def get(self, request, format=None):
-        serializer = NotificationSerializer(self.get_queryset(), many=True)
-        return Response(serializer.data)
 
+
+class FilterRead(NotifList):
+    model = Notification
+    def get_queryset(self):
+        queryset = Notification.objects.filter(
+            receiver=self.request.user.pk, state=True).order_by(
+            '-creation_date')
+        return queryset
