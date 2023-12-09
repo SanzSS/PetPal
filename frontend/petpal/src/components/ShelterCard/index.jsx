d97d014c31dd16bdff3427f281ef8ec6362a37c4
Sanzhar Shamshiyev <sanpos.kz@gmail.com>
2023-12-09 15:08:06 -0500

    notif basically done

--- frontend/petpal/src/components/ShelterCard/index.jsx
+++ frontend/petpal/src/components/ShelterCard/index.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
 
 const ShelterCard = ({shelter}) => {
+    console.log("hello");
     return <>
-        <Link to={`../listing/${shelter.id}`} class="card">
+        <Link to={`*`} className="card">
             {shelter.avatar.length !== 0 ? <img src={shelter.avatar} class="w-full h-[250px] object-cover" /> : <div className="w-full h-[250px] flex justify-center items-center"><p>No Image Available</p></div>}
-            <div class="p-2">
-                <span class="font-bold block capitalize">{ shelter.name }</span>
+            <div className="p-2">
+                <span className="font-bold block capitalize">{ shelter.name }</span>
             </div>
         </Link>
     </>
--- frontend/petpal/src/pages/ListShelters/index.jsx
+++ frontend/petpal/src/pages/ListShelters/index.jsx
@@ -2,22 +2,28 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useAuth } from '../../contexts/TokenContext';
 import ShelterCard from '../../components/ShelterCard';
+import { Link } from 'react-router-dom'
+
 
 
 const Shelters = () => {
     const [url, setUrl] = useState('http://127.0.0.1:8000/accounts/shelters/');
     const { token } = useAuth();
     const [shelters, setShelters] = useState([]);
-
+    const [next, setNext] = useState(null);
+    const [prev, setPrev] = useState(null);
+    
 
     useEffect(() => {axios.get(url, {
         headers: {
           Authorization: `Bearer ${token}`
         }
       }).then((response) => {
-        let temp = []
-        for (let i = 0; i < response.data.length; i++) {
-            temp.push(response.data[i]);
+        setNext(response.data.next);
+        setPrev(response.data.previous);
+        let temp = [...shelters]
+        for (let i = 0; i < response.data.results.length; i++) {
+            temp.push(response.data.results[i]);
           
         };
         setShelters(temp);
@@ -26,13 +32,42 @@ const Shelters = () => {
     .catch((error) => {
         console.log(error);
     });}, [url]);
-
     return <>
-        {shelters.map((shelter) => {
-            <ShelterCard shelter={shelter} />
-        })};
-    
-    
+    <body className="min-h-screen bg-blue1 flex flex-col">
+            <div className="flex flex-col items-center justify-center text-left">
+                <h1 className="lg:text-6xl my-12 text-blue3 font-extrabold md:text-6xl text-6xl self-center">
+                    Shelters
+                </h1>
+            </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 lg:p-2">
+
+        {shelters.map((shelt) => {
+                if (shelt.avatar == null){
+                    shelt.avatar = '';
+                console.log(shelt);}
+               return (
+                <>
+                    <ShelterCard key={shelt.pk} shelter={shelt} />
+                </>
+               );})}
+    </div>
+    <div className="flex flex-row gap-4">
+        <button onClick={() => {
+            if (prev != null) {
+                setUrl(prev);
+                setShelters([]);
+            }
+        }} className="bg-blue3 border border-blue3 text-white items-center font-bold py-2 px-4 rounded-md mt-8 mb-8 w-[6.5rem] hover:bg-white hover:text-blue3">Previous</button>
+        <button onClick={() => {
+            console.log(next);
+            if (next != null) {
+                setUrl(next);
+                setShelters([]);
+            }
+        }} className="bg-blue3 border border-blue3 text-white items-center font-bold py-2 px-4 rounded-md mt-8 mb-8 w-[6.5rem] hover:bg-white hover:text-blue3">Next</button>
+        </div>
+         
+        </body>
     </>
 
     
