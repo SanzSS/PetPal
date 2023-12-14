8d842dc65c7d27988246aa73db06ee4a4fc5b202
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-12-14 12:52:44 -0500

    in progress pagination

--- frontend/petpal/src/pages/ListApplication/index.jsx
+++ frontend/petpal/src/pages/ListApplication/index.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import { useAuth } from '../../contexts/TokenContext';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import { useUserType } from '../../contexts/UserTypeContext';
 import Application from '../../components/Application';
 
@@ -9,14 +9,30 @@ const ListApplications = () => {
     let navigate = useNavigate();
     const { token } = useAuth();
     const { userType } = useUserType();
-    // const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
     const [applications, setApplications] = useState([]);
     const [totalPages, setTotalPages] = useState(1);
     const [input, setInput] = useState('');
     const [allFilters, setAllFilters] = useState({});
 
+    const query = useMemo(() => ({
+        page: parseInt(searchParams.get("page") ?? 1),
+        filter: searchParams.get("filter") ?? '',
+        sort: searchParams.get("sort") ?? 'create_time',
+    }), [searchParams]);
+
     useEffect(() => {
-        axios.get(`http://127.0.0.1:8000/applications/`, 
+        const params = new URLSearchParams();
+
+        Object.keys(query).forEach((key) => {
+            if (query[key]) {
+                params.append(key, query[key]);
+            }
+        });
+
+        let url = `http://127.0.0.1:8000/applications/?${params}`;
+
+        axios.get(url, 
         {headers: {
             "Authorization": `Bearer ${token}`
             }
@@ -50,6 +66,28 @@ const ListApplications = () => {
             
             {Array.isArray(applications) && applications.map((application) => (
             <Application application={application} key={application.id} />))}
+
+<div className="text-center flex justify-center mt-4">
+                        {query.page > 1 ? <a onClick={() => setSearchParams({...query, page: query.page - 1})} className="button px-2 py-1 border-2 border-blue3 hover:text-blue3 font-extrabold cursor-pointer flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                                <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+                            </svg>
+                        </a> : <a className="text-white bg-gray-500 rounded h-8 shadow-md hover:border-2 px-2 py-1 border-2 border-gray-500 font-extrabold cursor-default flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                                <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+                            </svg>
+                        </a> }
+                        <p className="inline text-white bg-blue3 font-bold rounded h-8 shadow-md px-2 py-1 mx-2">Page {query.page}</p>
+                        {query.page < totalPages ? <a onClick={() => setSearchParams({...query, page: query.page + 1})} className="button px-2 py-1 border-2 border-blue3 hover:text-blue3 font-extrabold cursor-pointer flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                            </svg>
+                        </a> : <a className="text-white bg-gray-500 rounded h-8 shadow-md hover:border-2 px-2 py-1 border-2 border-gray-500 font-extrabold cursor-default flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                            </svg>
+                        </a> }
+                    </div>
             </div>
         </main>
         </>
