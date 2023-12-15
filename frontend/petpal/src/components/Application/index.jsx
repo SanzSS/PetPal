424553332ad5880dce3b5eac45780ce20f2818c5
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-12-14 19:41:59 -0500

    application styling

--- frontend/petpal/src/components/Application/index.jsx
+++ frontend/petpal/src/components/Application/index.jsx
@@ -103,14 +103,19 @@ const Application = ({application}) => {
         <div id="app-container" className="w-full flex items-center mb-4 mt-10">
             <details className="w-full flex items-center px-6 bg-white rounded-md border-2 border-blue3 border-solid">
                 <summary className="text-center cursor-pointer w-full text-lg mt-3 mb-3 font-bold capitalize" id="app">{application.pet.name}</summary>
-                <Link to={`/comments/${application.id}`} className="font-bold border-solid border-2 rounded-md cursor-pointer text-white text-xl bg-blue3 p-3 justify-center inline-flex items-center no-underline text-center hover:text-blue3 hover:border-blue3 hover:bg-white">
+                <Link to={`/comments/${application.id}`} className="mb-4 font-bold border-solid border-2 rounded-md cursor-pointer text-white text-xl bg-blue3 p-3 justify-center inline-flex items-center no-underline text-center hover:text-blue3 hover:border-blue3 hover:bg-white">
                     Chat
                 </Link>
-                <p className="text-left">
+                {userType === 'shelter' && application.status === 'pending' ? 
+                (<Link to={`/user/${application.user.id}`} className='mb-4 font-bold border-solid border-2 rounded-md cursor-pointer text-white text-xl bg-blue3 p-3 justify-center inline-flex items-center no-underline text-center hover:text-blue3 hover:border-blue3 hover:bg-white'>
+                    See user</Link>) 
+                : userType === 'shelter' && application.status !== 'pending' ? (<></>) 
+                : (<Link to={`/listing/${application.pet.id}`} className='mb-4 font-bold border-solid border-2 rounded-md cursor-pointer text-white text-xl bg-blue3 p-3 justify-center inline-flex items-center no-underline text-center hover:text-blue3 hover:border-blue3 hover:bg-white'>
+                    See pet</Link>)}
+                <p className="text-left mb-4">
                     Date applied: {(new Date(application.date)).toDateString()}
                 </p>
-                {userType === 'shelter' ? (<Link to={`/user/${application.user.id}`}>See user</Link>) : (<Link to={`/listing/${application.pet.id}`}>See pet</Link>)}
-                <p>
+        <p>
             Application Status: 
         </p>
     {userType === 'shelter' ? 
@@ -134,42 +139,42 @@ const Application = ({application}) => {
             Your address: <span>*</span>
         </p>
         <div>
-            <p type="text" name="question1" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[1]}</p>
+            <p type="text" name="question1" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[1]}</p>
         </div>
 
                 <p>
                     City: <span>*</span>
                 </p>
                 <div>
-                    <p type="text" name="question2" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[2]}</p>
+                    <p type="text" name="question2" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[2]}</p>
                 </div>
 
                 <p>
                     Postal Code: <span>*</span>
                 </p>
                 <div>
-                    <p type="text" name="question3"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[3]}</p>
+                    <p type="text" name="question3" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[3]}</p>
                 </div>
 
                 <p>
                     Phone number: <span>*</span>
                 </p>
                 <div>
-                    <p type="text" name="question4"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[4]}</p>
+                    <p type="text" name="question4" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[4]}</p>
                 </div>
 
                 <p>
                     Email: <span>*</span>
                 </p>
                 <div>
-                    <p type="email" name="question5"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[5]}</p>
+                    <p type="email" name="question5" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[5]}</p>
                 </div>
 
                 <p>
                     Are you 21 years of age and over?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question6"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
+                    <p name="question6" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
                     {answersDict[6]}
                     </p>
                 </div>
@@ -179,14 +184,14 @@ const Application = ({application}) => {
                     Who will be primarily responsible for the care of this pet?
                 </p>
                 <div>
-                    <p name="question7"  id="" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[7]}</p>
+                    <p name="question7" id="application-answer" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[7]}</p>
                 </div>
 
                 <p>
                     Do you live in a house, apartment, condominium or townhouse?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question8"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
+                    <p name="question8" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
                     {answersDict[8]}
                     </p>
                 </div>
@@ -195,7 +200,7 @@ const Application = ({application}) => {
                     Do you own or rent your home?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question9"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
+                    <p name="question9" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
                     {answersDict[9]}
                     </p>
                 </div>
@@ -204,14 +209,14 @@ const Application = ({application}) => {
                     How long have you lived at your current address (in years)?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question10"  type="number" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[10]}</p>
+                    <p name="question10" id="application-answer" type="number" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[10]}</p>
                 </div>
 
                 <p>
                     Do you have plans to move from your current address within the next 3 months?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question11"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
+                    <p name="question11" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
                     {answersDict[11]}
                     </p>
                 </div>
@@ -220,7 +225,7 @@ const Application = ({application}) => {
                     Do you have a securely fenced in yard?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question12"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
+                    <p name="question12" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
                     {answersDict[12]}
                     </p>
                 </div>
@@ -229,7 +234,7 @@ const Application = ({application}) => {
                     Do you have a pool?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question13"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
+                    <p name="question13" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
                     {answersDict[13]}
                     </p>
                 </div>
@@ -238,42 +243,42 @@ const Application = ({application}) => {
                     Why have you chosen this particular dog?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question14"  id="" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[14]}</p>
+                    <p name="question14" id="application-answer" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[14]}</p>
                 </div>
                 
                 <p>
                     Please explain your reasons for wanting to adopt a dog: <span>*</span>
                 </p>
                 <div>
-                    <p name="question15"  id="" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[15]}</p>
+                    <p name="question15" id="application-answer" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[15]}</p>
                 </div>
 
                 <p>
                     For whom are you adopting a dog? For you or for someone else? (If for someone else, who?): <span>*</span>
                 </p>
                 <div>
-                    <p name="question16"  id="" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[16]}</p>
+                    <p name="question16" id="application-answer"cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[16]}</p>
                 </div>
 
                 <p>
                     What experience do you have as a dog owner? If you have been a dog owner, please tell us about your previous dogs: <span>*</span>
                 </p>
                 <div>
-                    <p name="question17"  id="" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white">{answersDict[17]}</p>
+                    <p name="question17" id="application-answer"cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white">{answersDict[17]}</p>
                 </div>
 
                 <p>
                     How long has it been since you've had a dog in your life?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question18"  type="number" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[18]}</p>
+                    <p name="question18"  type="number" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[18]}</p>
                 </div>
 
                 <p>
                     Do you have dog training experience? And if so, what training do you have?: <span>*</span>
                 </p>
                 <div>
-                    <p name="question19"  id="" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[19]}</p>
+                    <p name="question19"  id="application-answer" cols="80" rows="10" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>{answersDict[19]}</p>
                 </div>
 
                 <p>
@@ -281,7 +286,7 @@ const Application = ({application}) => {
 
                 </p>
                 <div>
-                    <p name="question20"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
+                    <p name="question20" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
                     {answersDict[20]}
                     </p>
                 </div>
@@ -291,7 +296,7 @@ const Application = ({application}) => {
                 </p>
 
                 <div>
-                    <p name="question21"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
+                    <p name="question21" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
                     {answersDict[21]}
                     </p>
                 </div>
@@ -301,7 +306,7 @@ const Application = ({application}) => {
                 </p>
 
         <div>
-            <p name="question22"  className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
+            <p name="question22" id="application-answer" className="rounded-md shadow-md p-3 border-solid border-blue3 border-2 bg-white" required>
             {answersDict[22]}
             </p>
         </div>
