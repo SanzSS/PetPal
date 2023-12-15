c604e3a87c232c4448b9f29f1650ecc88c7a5480
Sanzhar Shamshiyev <sanpos.kz@gmail.com>
2023-12-15 11:51:00 -0500

    s

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # React server
+    "http://localhost:3000", "http://3.16.70.156", "http://3.16.70.156:80"  # React server
 ]
 
 
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-8!&34ftj^s*uys-fbos90x&6%c9#6fukq05+l3^4sx+u-dmgke
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://3.16.70.156/']
+ALLOWED_HOSTS = ['3.16.70.156']
 
 
 # Application definition
